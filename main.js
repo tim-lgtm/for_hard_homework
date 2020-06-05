0049a07389aaa6074922c743c27c4c29e6ca0ec7
tima <tima_3015@mail.ru>
2020-06-05 17:29:23 +0300

    это было пипец как сложно

---
+++ main.js
@@ -0,0 +1,11 @@
+let num = 266219;
+let result = Array.from(`${num}`).reduce((a, b) =>a * b);
+result =  result ** 3;
+console.log(result);
+result = result.toString();
+console.log(typeof result);
+console.log(result [0]);
+console.log(result [1]);
+
+
+
