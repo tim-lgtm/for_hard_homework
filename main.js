df093adf547ef314d189d12fa6f2e2e2b723a05b
tima <tima_3015@mail.ru>
2020-06-08 19:49:25 +0300

    Не всё сделал

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 'use strict';
+// общие данные данные для всех заданий 
+let question = prompt('Введи язык')
+let lang = question;
 
-// let question = prompt('Введи язык')
-// console.log( typeof question);
-// let lang = question;
-// console.log(typeof lang);
-
-// if( lang == 'ru') {
-//   console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-// } else if (lang == 'en') {
-//   console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-// }
-
-// switch(lang) {
-//   case 'ru':
-//     console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-//     break;
-//    case 'en':
-//     console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-// }
-
+// a
+if( lang == 'ru') {
+  console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+} else if (lang == 'en') {
+  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+}
+//b
+switch(lang) {
+  case 'ru':
+    console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+    break;
+   case 'en':
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+}
+//c не выполнил чуть похже скину...ещё подумаю как это можно сделать 
 let result = [
   ['Понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
   ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
@@ -32,10 +31,10 @@ console.log(result[1]);
 
 
 
-//второе задание 
-// let ask = prompt('Ваше имя')
-// let namePerson = ask;
-// // console.log(namePerson);
-// let result = ( namePerson == 'Артём') ? 'директор' :
-//  (namePerson == 'Максим') ? 'преподаватель' : 'студент' ;
-// console.log(result);
+//второе задание  
+let ask = prompt('Ваше имя')
+let namePerson = ask;
+// console.log(namePerson);
+let result = ( namePerson == 'Артём') ? 'директор' :
+ (namePerson == 'Максим') ? 'преподаватель' : 'студент' ;
+console.log(result);
