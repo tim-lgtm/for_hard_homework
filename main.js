4c8b3911af47457dae211ae90d7744c8d319472b
tima <tima_3015@mail.ru>
2020-06-08 23:33:31 +0300

    Пофиксил

--- main.js
+++ main.js
@@ -4,25 +4,28 @@ let question = prompt('Введи язык')
 let lang = question;
 
 // a
-if( lang == 'ru') {
-  console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-} else if (lang == 'en') {
-  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-}
-//b
-switch(lang) {
-  case 'ru':
-    console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-    break;
-   case 'en':
-    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-}
+// if( lang == 'ru') {
+//   console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+// } else if (lang == 'en') {
+//   console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+// }
+// //b
+// switch(lang) {
+//   case 'ru':
+//     console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+//     break;
+//    case 'en':
+//     console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+// }
 //c не выполнил чуть похже скину...ещё подумаю как это можно сделать 
-let result = [
+let arr = [
   ['Понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
   ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 ]
-console.log(result[1]);
+// console.log(lang);
+let result = (lang == 'en') ? (arr[1]) : (arr[0]);
+console.log('result: ', result);
+
 
 
 
@@ -32,9 +35,9 @@ console.log(result[1]);
 
 
 //второе задание  
-let ask = prompt('Ваше имя')
-let namePerson = ask;
-// console.log(namePerson);
-let result = ( namePerson == 'Артём') ? 'директор' :
- (namePerson == 'Максим') ? 'преподаватель' : 'студент' ;
-console.log(result);
+// let ask = prompt('Ваше имя')
+// let namePerson = ask;
+// // console.log(namePerson);
+// let result = ( namePerson == 'Артём') ? 'директор' :
+//  (namePerson == 'Максим') ? 'преподаватель' : 'студент' ;
+// console.log(result);
