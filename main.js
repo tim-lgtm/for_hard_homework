b2872f47a1666138e257108194a036da11a8715c
tima <tima_3015@mail.ru>
2020-06-08 19:49:07 +0300

    Не всё сделал , проверь по возможности

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
-let num = 266219;
-let result = Array.from(`${num}`).reduce((a, b) =>a * b);
-result =  result ** 3;
-console.log(result);
-result = result.toString();
-console.log(typeof result);
-result.slice(0,2)
-console.log(result.substr(0,2));
+'use strict';
+
+// let question = prompt('Введи язык')
+// console.log( typeof question);
+// let lang = question;
+// console.log(typeof lang);
+
+// if( lang == 'ru') {
+//   console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+// } else if (lang == 'en') {
+//   console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+// }
+
+// switch(lang) {
+//   case 'ru':
+//     console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+//     break;
+//    case 'en':
+//     console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+// }
+
+let result = [
+  ['Понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
+  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+]
+console.log(result[1]);
 
 
 
+
+
+
+
+
+//второе задание 
+// let ask = prompt('Ваше имя')
+// let namePerson = ask;
+// // console.log(namePerson);
+// let result = ( namePerson == 'Артём') ? 'директор' :
+//  (namePerson == 'Максим') ? 'преподаватель' : 'студент' ;
+// console.log(result);
