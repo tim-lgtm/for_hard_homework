9916b05de9d9b4c1045814d935423f8e588db09b
tima <tima_3015@mail.ru>
2020-06-10 01:15:30 +0300

    поправил ещё раз

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-function Tim(a) {
-  if (typeof a == 'string' &&  a.length > 30 ){
-    return a.trim().substring(0,30) + '...';
+// function Tim(a) {
+//   if (typeof a == 'string' &&  a.length > 30 ){
+//     return a.trim().substring(0,30) + '...';
       
-  }else{ 
-    return 'Введите что то другое';
-    } 
-}
+//   }else{ 
+//     return 'Введите что то другое';
+//     } 
+// }
 
-let result = Tim('    Я понимаю, что этот код мягко говоря не очень, но он работает и я не понимаю почему он это делает , поэтому хотел бы сразу  спросить, как можно ещё решить эту задачу');
+// let result = Tim(11111);
+// console.log(result );
+
+function Tim(a) {
+  if (typeof a == 'string'){
+    let x = a.trim();
+    if(a.length > 30 ){
+      return a.substring(0, 30) + '...';
+    }else {
+    return x ;
+    }
+  }else{
+    return 'Подумай ещё ';
+  }
+}
+let result = Tim(223);
 console.log(result );
 
 
