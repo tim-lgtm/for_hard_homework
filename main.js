9d5aceaeb35df2719b7de4d5f5f031d96422054b
tima <tima_3015@mail.ru>
2020-06-09 16:35:41 +0300

    уз

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-let num = 266219;
-let result = Array.from(`${num}`).reduce((a, b) =>a * b);
-result =  result ** 3;
-console.log(result);
-result = result.toString();
-console.log(typeof result);
-result.slice(0,2)
-console.log(result.substr(0,2));
+function Tim(a) {
+  if (typeof a == 'string' && a.length > 30 ){
+    return a.trim().substring(0,30);
+      
+  }else{ 
+    return 'Введите что то другое';
+    } 
+}
 
-
+let result = Tim('   Я понимаю, что этот код мягко говоря не очень, но он работает и я не понимаю почему он это делает , поэтому хотел бы сразу  спросить, как можно ещё решить эту задачу   ');
+console.log(result + '...');
 
